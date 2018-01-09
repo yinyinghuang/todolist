f77e954c29db79da7e4648c39daa2ad0acfa9109
yinyinghuang <934646546@qq.com>
2018-01-09 23:05:45 +0800

    defined sign in action

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,30 @@
 const User = require('../models/User');
 
-module.exports = User;
+module.exports = {
+	async signIn (ctx) {
+     let result = {
+         success: false,
+         message: '用户不存在'
+     };
+     //从请求体中获得参数
+     const { username,  password } = {username:'test',password:'123456'};
+     //检查数据库中是否存在该用户名
+     await User.findOne({
+         username
+     }, (err, user) => {
+         if (err) {
+             throw err;
+         }
+         if (!user) {
+             ctx.body = result;
+         } else {
+             //判断密码是否正确
+             if (password === user.password) {
+                 ctx.body = {success: true, message: '登入成功'}
+             } else {
+                 ctx.body = {success: false, message: '密码错误'}
+             }
+         }
+     })
+ }
+};
