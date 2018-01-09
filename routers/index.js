36260e2074ebf20a3b0b7cb7cf5ef798d757a421
yinyinghuang <934646546@qq.com>
2018-01-09 23:09:46 +0800

    place the server router code

--- routers/index.js
+++ routers/index.js
@@ -1,24 +1,15 @@
 const router=require('koa-router')();
 const path = require('path');
 
-const User = require('../models/User.js');
-const Todo = require('../models/Todo.js');
+const User = require('../controllers/UserController.js');
+const Todo = require('../controllers/TodoController.js');
 
-router.post('/api/user',async ctx =>{
-	ctx.body= 'sss';
-});
+
+router.post('/user',User.signIn);
 
 router.get('/api/todolist',async ctx =>{
-	let todos='';
-	User.find((err,doc) => {
-		todos = doc;
-	});
-	console.log('todos',todos);
-	ctx.body= todos;
-});
-
-router.get('/',async ctx => {
-	ctx.body = await fs.readFileSync(path.resolve(__dirname, 'build','index.html'),'utf8');
+	
+	ctx.body= '';
 });
 
 module.exports = router;
